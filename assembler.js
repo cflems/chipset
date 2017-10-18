101bfe65fb785388895c56f348eb124f0e899af0
Carson Fleming <cflems@cflems.net>
2017-10-18 00:33:52 -0400

    Hardcoded reserved symbols into table

--- assembler.js
+++ assembler.js
@@ -21,7 +21,33 @@ let basefile = process.argv[2].substr(0, process.argv[2].lastIndexOf('.asm')),
     outfile = basefile + '.hack';
 
 let inputs = fs.readFileSync(infile, 'utf-8').split('\n').map(ln => ln.replace(/\/\/.*/g, '').trim());
-let symbolT = {};
+
+let symbolT = {
+  'SP': 0x0,
+  'LCL': 0x1,
+  'ARG': 0x2,
+  'THIS': 0x3,
+  'THAT': 0x4,
+  'SCREEN': 0x4000,
+  'KBD': 0x6000,
+  'R0': 0x0,
+  'R1': 0x1,
+  'R2': 0x2,
+  'R3': 0x3,
+  'R4': 0x4,
+  'R5': 0x5,
+  'R6': 0x6,
+  'R7': 0x7,
+  'R8': 0x8,
+  'R9': 0x9,
+  'R10': 0xa,
+  'R11': 0xb,
+  'R12': 0xc,
+  'R13': 0xd,
+  'R14': 0xe,
+  'R15': 0xf,
+};
+
 let varcounter = 16;
 let outputs = [];
 

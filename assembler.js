e6c9f7cb48ea558d80ee340f8d4c972e72ae12b0
Carson Fleming <cflems@cflems.net>
2017-10-19 14:12:57 -0400

    Kinder line max length in assembler code

--- assembler.js
+++ assembler.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 function quit (message) {
   console.error('[Error] '+message);
@@ -6,13 +7,17 @@ function quit (message) {
 }
 
 function makebinstr (num) {
-  if (num < 0 || num > 32767) quit('Integer out of bounds: '+num+'. Literals must be unsigned and are constrained to 15 bits.');
+  if (num < 0 || num > 32767) quit('Integer out of bounds: '+num
+    +'. Literals must be unsigned and are constrained to 15 bits.');
   let binstr = num.toString(2);
   while (binstr.length < 16) binstr = '0'+binstr;
   return binstr;
 }
 
-if (process.argv.length < 3) quit('A file input was expected.');
+if (process.argv.length < 3) {
+  console.log('Usage: '+path.basename(process.argv[0])+' '+path.basename(process.argv[1])+' <file.asm>');
+  quit('A file input was expected.');
+}
 if (!process.argv[2].endsWith('.asm'))
   quit('Only .asm files are accepted.');
 
@@ -20,7 +25,9 @@ let basefile = process.argv[2].substr(0, process.argv[2].lastIndexOf('.asm')),
     infile = basefile + '.asm',
     outfile = basefile + '.hack';
 
-let inputs = fs.readFileSync(infile, 'utf-8').split('\n').map(ln => ln.replace(/\/\/.*/g, '').trim());
+let inputs = fs.readFileSync(infile, 'utf-8')
+               .split('\n')
+               .map(ln => ln.replace(/\/\/.*/g, '').trim());
 
 let symbolT = {
   'SP': 0x0,
@@ -56,7 +63,8 @@ for (let i = 0, pc = 0; i < inputs.length; i++) {
   if (inputs[i].length < 1) continue;
   if (inputs[i].endsWith(':') && !inputs[i].startsWith('@')) {
     let label = inputs[i].substr(0, inputs[i].length-1);
-    if (label in symbolT) quit('Redaclaration of label '+label+' in line '+(i+1)+'.');
+    if (label in symbolT) quit('Redaclaration of label '
+                                +label+' in line '+(i+1)+'.');
     symbolT[label] = pc;
   } else pc++;
 }
@@ -82,24 +90,32 @@ for (let i = 0; i < inputs.length; i++) {
     // c-inst
     let working = ['1','1','1','0','0','0','0','0','0','0','0','0','0','0','0','0'];
     if (inputs[i].indexOf(';') != -1) {
-      let jstmt = inputs[i].substr(inputs[i].indexOf(';')+1).trim().toUpperCase();
+      let jstmt = inputs[i].substr(inputs[i].indexOf(';')+1)
+                  .trim().toUpperCase();
       if (['JLT', 'JLE', 'JNE', 'JMP'].indexOf(jstmt) != -1) working[13] = '1';
       if (['JEQ', 'JLE', 'JGE', 'JMP'].indexOf(jstmt) != -1) working[14] = '1';
       if (['JGT', 'JGE', 'JNE', 'JMP'].indexOf(jstmt) != -1) working[15] = '1';
-      if (working[13] == '0' && working[14] == '0' && working[15] == '0' && jstmt.length > 0)
-        quit('Invalid syntax in "'+inputs[i]+'": '+jstmt+' is not a valid jump command.');
+      if (working[13] == '0' && working[14] == '0'
+          && working[15] == '0' && jstmt.length > 0)
+        quit('Invalid syntax in "'+inputs[i]+'": '
+             +jstmt+' is not a valid jump command.');
     }
-    
+
     // jumps are handled. use command before semicolon
     let leftcmd = inputs[i].split(';')[0].trim();
-    
+
     // TODO: work = commands format: MD=alushit
     // setter commands
     if (leftcmd.indexOf('=') > -1) {
-      let assign = leftcmd.substr(0, leftcmd.indexOf('=')).replace(/\s/g, '').toUpperCase();
-      if (assign.length > 3) quit('(Line '+(i+1)+'): There are a maximum of 3 things to be assigned.');
+      let assign = leftcmd.substr(0, leftcmd.indexOf('='))
+                   .replace(/\s/g, '').toUpperCase();
+      if (assign.length > 3)
+        quit('(Line '+(i+1)+'): '
+             +'There are a maximum of 3 things to be assigned.');
       for (let j = 0; j < assign.length; j++) {
-        if (['A', 'D', 'M'].indexOf(assign[j]) < 0) quit('(Line '+(i+1)+'): You cannot assign nonexistant register '+assign[j]+'.');
+        if (['A', 'D', 'M'].indexOf(assign[j]) < 0)
+          quit('(Line '+(i+1)+'): '
+                +'You cannot assign nonexistant register '+assign[j]+'.');
         if (assign[j] == 'A' && working[10] == '0') working[10] = '1';
         else if (assign[j] == 'D' && working[11] == '0') working[11] = '1';
         else if (assign[j] == 'M' && working[12] == '0') working[12] = '1';
@@ -110,7 +126,8 @@ for (let i = 0; i < inputs.length; i++) {
     // a-bit processing
     let alucmd = leftcmd.substr(leftcmd.indexOf('=')+1).trim().toUpperCase();
     if (alucmd.indexOf('A') > -1 && alucmd.indexOf('M') > -1)
-      quit('(Line '+(i+1)+'): You can\'t use both A and M in your input to the ALU.');
+      quit('(Line '+(i+1)+'):'
+            +' You can\'t use both A and M in your input to the ALU.');
     else if (alucmd.indexOf('M') > -1) working[3] = '1';
     alucmd = alucmd.replace(/A|M/g, 'X').replace(/\s/g, '');
 
@@ -173,12 +190,15 @@ for (let i = 0; i < inputs.length; i++) {
         working[5]=working[7]=working[9]='1';
         break;
       default:
-        quit('(Line '+(i+1)+'): "'+alucmd+'" is not a valid command to the ALU.');
+        quit('(Line '+(i+1)+'): "'
+             +alucmd+'" is not a valid command to the ALU.');
     }
- 
+
     // push the line
-    outputs.push(working.join('')); 
+    outputs.push(working.join(''));
   }
 }
 
-fs.writeFileSync(outfile, outputs.join('\n')+'\n', {encoding: 'utf-8', mode: 0o644, flag: 'w'});
+fs.writeFileSync(outfile,
+                 outputs.join('\n')+'\n',
+                 {encoding: 'utf-8', mode: 0o644, flag: 'w'});
